cfd34b50521e1fc8c199afb738b9d2a083468c09
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 17:43:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  return `It\'s ${days} until ${holiday}!`;
+  return `It\'s ${days} days until ${holiday}!`;
 }
