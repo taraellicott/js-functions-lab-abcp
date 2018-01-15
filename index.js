c5199d69d2387056d1bde70888ce973f225b97ab
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 17:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function happyHolidays() {
 
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
+  return `Happy holidays, ${name}!`;
 }
 
 
 function happyHolidayTo(holiday, name) {
-return "Happy ${holiday}, ${name}!";
+return `Happy ${holiday}, ${name}!`;
   
 }
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} until ${holiday}!";
+  return `It\'s ${days} until ${holiday}!`;
 }
